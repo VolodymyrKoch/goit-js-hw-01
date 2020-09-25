372819a406a9502b5ff8c48ce07f90dda71a12e7
VolodymyrKoch <67268447+VolodymyrKoch@users.noreply.github.com>
2020-09-25 19:22:09 +0300

    preater

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+const invoice = 100
+let stock = 100
+
+// Write code under this line
+let message = invoice >= stock ? "Заказ оформлен, с вами свяжется менеджер" : "На складе недостаточно товаров!"
+console.log(message)
+
+// stock = 50
+// message = invoice >= stock ? `Заказ оформлен, с вами свяжется менеджер` : `На складе недостаточно товаров!`
+// console.log(message)
